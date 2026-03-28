0531bbe3735478ce914176fd0a70299ef483edb2
Qenszu <opalusaktv@gmail.com>
2026-03-28 14:09:24 +0100

    Add local folder for photos, end backend

--- photos/urls.py
+++ photos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('api/photos/', views.PhotoList.as_view(), name='photo_list'),
+    path('map/', views.map_view, name='map_view')
 ]
--- photos/views.py
+++ photos/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Photo
 from .serializers import PhotoSerializer
+from django.shortcuts import render
 # Create your views here.
 
 class PhotoList(generics.ListCreateAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+
+def map_view(request):
+    return render(request, 'photos/map.html')
